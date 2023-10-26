e8eccae2544f7cde073682ce0d93bb88b3d9fd0d
MeiKatz <gregor.mitzka@gmail.com>
2023-10-26 18:59:50 +0200

    [feature] add status constants to class Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    const STATUS_NOT_STARTED = 1;
+    const STATUS_IN_PROGRESS = 2;
+    const STATUS_IN_REVIEW   = 3;
+    const STATUS_ON_HOLD     = 4;
+    const STATUS_COMPLETED   = 5;
+    const STATUS_CANCELLED   = 6;
+
     public function clientdata()
 	{
         return  $this->belongsTo(Client::class, 'client', 'id');
--- resources/views/components/project-status.blade.php
+++ resources/views/components/project-status.blade.php
@@ -1,25 +1,25 @@
 @switch ( $status )
-    @case(1)
+    @case( \App\Models\Project::STATUS_NOT_STARTED )
         <span class="badge btn-white">Not Started</span>
         @break
 
-    @case(2)
+    @case( \App\Models\Project::STATUS_IN_PROGRESS )
         <span class="badge bg-blue">In Progress</span>
         @break
 
-    @case(3)
+    @case( \App\Models\Project::STATUS_IN_REVIEW )
         <span class="badge bg-purple">In Review</span>
         @break
 
-    @case(4)
+    @case( \App\Model\Project::STATUS_ON_HOLD )
         <span class="badge bg-yellow">On Hold</span>
         @break
 
-    @case(5)
+    @case( \App\Models\Project::STATUS_COMPLETED )
         <span class="badge bg-green">Completed</span>
         @break
 
-    @case(6)
+    @case( \App\Models\Project::STATUS_CANCELLED )
         <span class="badge bg-dark">Cancelled</span>
         @break
 @endswitch
