2a084b81183d2a87034e3f9aa4ef466d15551f80
MeiKatz <gregor.mitzka@gmail.com>
2023-10-27 14:08:33 +0200

    [feature] add relationship '#invoice' to class InvoiceItem

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceItem extends Model
 {
     use HasFactory;
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = [
+        'invoice'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function invoice() {
+        return $this->belongsTo(
+            Invoice::class,
+            'invoiceid'
+        );
+    }
 }
