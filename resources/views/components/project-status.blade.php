57059a65680e3333acac4a3c4f349715f73ca6a0
MeiKatz <gregor.mitzka@gmail.com>
2023-10-27 21:52:09 +0200

    [feature] refactor component `<x-project-status />`

--- resources/views/components/project-status.blade.php
+++ resources/views/components/project-status.blade.php
@@ -1,25 +1,25 @@
-@switch ( $status )
-    @case( \App\Models\Project::STATUS_NOT_STARTED )
+@switch ( true )
+    @case( $project->is_not_started )
         <span class="badge btn-white">Not Started</span>
         @break
 
-    @case( \App\Models\Project::STATUS_IN_PROGRESS )
+    @case( $project->is_in_progress )
         <span class="badge bg-blue">In Progress</span>
         @break
 
-    @case( \App\Models\Project::STATUS_IN_REVIEW )
+    @case( $project->is_in_review )
         <span class="badge bg-purple">In Review</span>
         @break
 
-    @case( \App\Model\Project::STATUS_ON_HOLD )
+    @case( $project->is_on_hold )
         <span class="badge bg-yellow">On Hold</span>
         @break
 
-    @case( \App\Models\Project::STATUS_COMPLETED )
+    @case( $project->is_completed )
         <span class="badge bg-green">Completed</span>
         @break
 
-    @case( \App\Models\Project::STATUS_CANCELLED )
+    @case( $project->is_cancelled )
         <span class="badge bg-dark">Cancelled</span>
         @break
 @endswitch
