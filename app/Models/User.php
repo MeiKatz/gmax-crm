6b433c1cfd0fe78ca10b6916d1dc2ca8cedf52c9
MeiKatz <gregor.mitzka@gmail.com>
2023-10-30 12:06:14 +0100

    [feature] add scopes for staff and admin for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,16 +65,42 @@ class User extends Authenticatable
     ];
 
     /**
+     * Scope a query to only include admins.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAdmin( $query ) {
+        return $query->where(
+            'usertype',
+            self::USER_TYPE_ADMIN
+        );
+    }
+
+    /**
+     * Scope a query to only include users from staff.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeStaff( $query ) {
+        return $query->whereNot(
+            'usertype',
+            self::USER_TYPE_ADMIN
+        );
+    }
+
+    /**
      * @return bool
      */
     public function getIsAdminAttribute() {
-        return $this->usertype === self::USER_TYPE_ADMIN;
+        return $this->usertype == self::USER_TYPE_ADMIN;
     }
 
     /**
      * @return bool
      */
     public function getIsStaffAttribute() {
-        return $this->usertype === self::USER_TYPE_STAFF;
+        return $this->usertype != self::USER_TYPE_ADMIN;
     }
 }
